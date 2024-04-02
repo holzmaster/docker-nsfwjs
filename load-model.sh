da9a5bfe993f19b1249569896c79e0c636ce7021
holzmaster <holzmaster@outlook.com>
2024-04-02 03:41:49 +0200

    Fix URLs

--- load-model.sh
+++ load-model.sh
@@ -5,10 +5,10 @@ mkdir model
 
 pushd model
 
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard1of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard2of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard3of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard4of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard5of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/group1-shard6of6
-curl -OL https://github.com/infinitered/nsfwjs/raw/master/example/nsfw_demo/public/model/model.json
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard1of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard2of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard3of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard4of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard5of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/group1-shard6of6
+curl -OL https://github.com/infinitered/nsfwjs/raw/master/models/inception_v3/model.json
