037900c9ca53e007da680a671f5d52ce8cedbe15
holzmaster <holzmaster@outlook.com>
2024-04-02 03:43:14 +0200

    npm run format

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export default {
-	modelDir: process.env.MODEL_DIR ?? "node_modules/nsfwjs/dist/models/inception_v3",
+	modelDir:
+		process.env.MODEL_DIR ?? "node_modules/nsfwjs/dist/models/inception_v3",
 	port: Number(process.env.API_PORT ?? "8080"),
 	host: process.env.API_HOST ?? "0.0.0.0",
 };
